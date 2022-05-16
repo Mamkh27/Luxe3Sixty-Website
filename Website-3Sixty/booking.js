3462faaff6ac466e0e81ecaa41344ef0dc7156e4
mampz <mkheer@ucdavis.edu>
2022-05-16 01:01:20 -0700

    Created foundation for finding out Venue Mileage Costs
    Bug in parsing data/results in callback function of AJAX request

---
+++ Website-3Sixty/booking.js
@@ -0,0 +1,50 @@
+function getQuoteBtnPressed(){
+    let address = document.getElementById("venue-address").value;
+    let homeaddress = {
+        latitude: 38.684292,
+        longitude: -121.535684
+    }
+    // conversion factor
+const factor = 0.621371
+  let differenceInMiles = 0;
+console.log("button pressed");
+console.log(`User address is ${address}`);
+
+//address geocoding
+
+$.ajax({
+    url: 'https://api.positionstack.com/v1/forward',
+    data: {
+      access_key: 'd8ace25125b394dca1d0f92da3a00ee8',
+      query: address,
+      limit: 1
+    }
+  }).done(function(data) {
+      console.log("Now printing...");
+    console.log(data.parse());
+    // console.log(`the latitude is ${data[0].latitude}`);
+
+    // Find the mileage between home and inputed address
+    // differenceInMiles = factor * calcCrow(homeaddress.latitude, homeaddress.longitude, data['latitude'], data['longitude']);
+    // console.log(`The amount of miles between the home address and ${address} is ${differenceInMiles}`);
+  });
+  }
+
+  function calcCrow(lat1, lon1, lat2, lon2) {
+    var R = 6371 // km
+    var dLat = toRad(lat2 - lat1)
+    var dLon = toRad(lon2 - lon1)
+    var lat1 = toRad(lat1)
+    var lat2 = toRad(lat2)
+
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2)
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    var d = R * c
+    return d
+}
+
+// Converts numeric degrees to radians
+function toRad(Value) {
+    return Value * Math.PI / 180
+}
